c41cff7de0026f926c54e6ee871858bda4796215
R0SD1N3V <ruslan.dinev.24@trakia-uni.bg>
2026-04-08 14:33:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const foodItems = [
     { id: 1, name: "Пица Маргарита", price: 12.90 },
     { id: 2, name: "Бургер Класик", price: 9.50 },
     { id: 3, name: "Цезар Салата", price: 8.20 }
+    { id: 4, name: "Шоколадова торта", price: 6.30 },
+    { id: 5, name: "Чийзкейк", price: 4.10 },
+    { id: 6, name: "Макарони", price: 3.20 }
+    { id: 7, name: "Палачинки", price: 5.30 }
 ];
 
 const app = document.getElementById('app');
